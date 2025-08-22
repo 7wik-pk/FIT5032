79ea15236635fbc1bc68b4f8fa9ccb70428a510c
Sathwik Kallapur <skal0045@student.monash.edu>
2025-08-22 21:40:34 +1000

    use DataTable to display user data

--- Skallapur-library/src/components/Form.vue
+++ Skallapur-library/src/components/Form.vue
@@ -67,19 +67,16 @@
             </div>
 
             <div class="row mt-5" v-if="submittedCards.length">
-              <div class="d-flex flex-wrap justify-content-start">
-                  <div v-for="(card, index) in submittedCards" :key="index" class="card m-2" style="width: 18rem;">
-                    <div class="card-header">
-                        User Information
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">Username: {{ card.username }}</li>
-                        <li class="list-group-item">Password: {{ card.password }}</li>
-                        <li class="list-group-item">Australian Resident: {{ card.isAustralian ? 'Yes' : 'No' }}</li>
-                        <li class="list-group-item">Gender: {{ card.gender }}</li>
-                        <li class="list-group-item">Reason: {{ card.reason }}</li>
-                    </ul>
-                  </div>
+              <div class="col-12">
+
+                <DataTable :value="submittedCards" tableStyle="min-width: 50rem">
+                  <Column field="username" header="Username"></Column>
+                  <Column field="password" header="Password"></Column>
+                  <Column field="isAustralian" header="Australian Resident"></Column>
+                  <Column field="gender" header="Gender"></Column>
+                  <Column field="reason" header="Reason"></Column>
+                </DataTable>
+
               </div>
             </div>
 
@@ -95,6 +92,8 @@
 <script setup>
 
   import { ref } from 'vue';
+  import DataTable from 'primevue/datatable';
+  import Column from 'primevue/column';
 
   const formData = ref({
       username: '',
--- Skallapur-library/src/main.js
+++ Skallapur-library/src/main.js
@@ -1,8 +1,17 @@
 // import './assets/main.css'
 // import './style.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
+const app = createApp(App)
+
+app.use(PrimeVue, {
+  theme: Aura
+})
+
 createApp(App).mount('#app')
+
